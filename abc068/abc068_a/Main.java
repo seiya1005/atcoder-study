d065d2ab94f1659abaabe1456db5680dc99989ea
seiyasasaki <seiya1005.works@gmail.com>
2023-07-28 23:12:51 +0900

    add problem

---
+++ abc068/abc068_a/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+    Scanner scan = new Scanner(System.in);
+    String str = scan.nextLine();
+
+    System.out.println("ABC" + str);
+    
+    scan.close();
+  }
+}
